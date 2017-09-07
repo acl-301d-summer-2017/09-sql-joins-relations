0913ed169de18b6732f74af887a8b6c648ce1df9
Robin Miller <rmillerpdx@gmail.com>
2017-09-07 16:34:08 -0700

    finish todo's , code still broken

--- starter-code/server.js
+++ starter-code/server.js
@@ -69,7 +69,7 @@ app.post('/articles', function(request, response) {
     // DONE---TODO: Add the data from our new article, including the author_id, as data for the SQL query.
     client.query(
       `INSERT INTO article (author_id, title, category, "publishedOn", body ) VALUES ($1, $2, $3, $4, $5)`,
-      [author_id, request.body.title, request.body.category, request.body.publishedOn, request.body.body],
+      [request.body.author_id, request.body.title, request.body.category, request.body.publishedOn, request.body.body],
 
       function(err) {
         if (err) console.error(err);
@@ -84,16 +84,27 @@ app.put('/articles/:id', function(request, response) {
   // an author_id property, so we can reference it from the request.body.
   // TODO: Add the required values from the request as data for the SQL query to interpolate
   client.query(
-    ``,
-    []
+    `UPDATE authors 
+    SET
+    author = "$1", authorUrl = "$2" 
+    WHERE author_id = request.body.article_id `,
+    [request.body.author_id, request.body.article_id]
   )
     .then(function() {
     // TODO: Write a SQL query to update an article record. Keep in mind that article records
     // now have an author_id, in addition to title, category, publishedOn, and body.
     // TODO: Add the required values from the request as data for the SQL query to interpolate
       client.query(
-        ``,
-        []
+        `UPDATE articles
+        SET
+        author_id = "$1"
+        title = "$2"
+        category = "$3"
+        publishedOn = "$4"
+        body = "$5"
+        WHERE article_id = request.params.id
+        `,
+        [request.body.author_id, request.body.title, request.body.category, request.body.publishedOn, request.body.body]
       )
     })
     .then(function() {
